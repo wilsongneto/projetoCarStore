61727914a4e0dd7e586d3443b8eebd854e9a5a4f
Wilson <109673935+wilsongneto@users.noreply.github.com>
2026-05-04 11:04:19 -0300

    js

--- script/script.js
+++ script/script.js
@@ -2,4 +2,8 @@ function salvarCarros(event) {
     event.preventDefault();
 
     let titulo = document.getElementById('title').value;
+    let preco = document.getElementById('preco').value;
+    let marca = document.getElementById('marca').value;
+    let modelo = document.getElementById('modelo').value;
+
 }

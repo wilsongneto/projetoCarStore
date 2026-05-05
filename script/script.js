ff147d20c9f1dcfeb50c5c0c5284cbaafbc9f592
Wilson <109673935+wilsongneto@users.noreply.github.com>
2026-05-05 08:45:44 -0300

    js

--- script/script.js
+++ script/script.js
@@ -1,4 +1,5 @@
 function salvarCarros(event) {
+    //impedir de recarregar a página
     event.preventDefault();
 
     let titulo = document.getElementById('title').value;
@@ -6,4 +7,47 @@ function salvarCarros(event) {
     let marca = document.getElementById('marca').value;
     let modelo = document.getElementById('modelo').value;
 
+    let cambioSelecionado = document.querySelector('input[name="marcha"]:checked');
+
+    let cambio = cambioSelecionado ? cambioSelecionado.id : "Não informado";
+
+        let carro = {
+        titulo,
+        preco,
+        marca,
+        modelo, 
+        cambio
+    };
+
+    let carros = JSON.parse(localStorage.getItem("carros")) || [];
+    carros.push(carro);
+    localStorage.setItem("carros", JSON.stringify(carros));
+
+    adicionarNaTela(carro);
+
+    document.querySelector("form").reset();
+}
+
+function adicionarNaTela(carro) {
+    let lista = document.getElementById('listarCarros');
+    let card = document.createElement('div');
+    card.classList.add('card');
+
+    card.innerHTML = `
+    <h3>${carro.titulo}</h3>
+    <p><strong>Preço:</strong> R$ ${carro.preco}</p>
+    <p><strong>Marca:</strong> ${carro.marca}</p>
+    <p><strong>Modelo:</strong> ${carro.modelo}</p>
+    <p><strong>Câmbio:</strong> ${carro.cambio}</p>
+    `;
+
+    lista.appendChild(card);
+}
+
+window.onload = function () {
+    let carros = JSON.parse(localStorage.getItem("carros")) || [];
+
+    carros.forEach(carro => {
+        adicionarNaTela(carro);
+    });
 }
